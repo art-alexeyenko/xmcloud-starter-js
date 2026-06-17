2d9bfbc46fdb074f1996f296095ceb3e85f5eb66
Artem Alexeyenko <ala@sitecore.net>
2026-06-17 16:27:54 -0400

    temp test with pre-set metadata

--- examples/angular-beta/sitecore.cli.config.ts
+++ examples/angular-beta/sitecore.cli.config.ts
@@ -1,14 +1,12 @@
-import { defineCliConfig, generateMetadata } from '@sitecore-content-sdk/angular/config-cli';
 /**
  * Sitecore CLI configuration (Node / build-time only). This file is not part of the Angular
  * compiler `include` set and is only loaded by `sitecore-tools`.
+ *
+ * Note: Using a minimal config due to canary SDK limitations (dist-node folder not yet available)
  */
-export default defineCliConfig({
-  build: {
-    commands: [generateMetadata()],
-  },
+export default {
   componentMap: {
     paths: ['src/app/components'],
     exclude: ['**/*.spec.ts'],
   },
-});
+};
