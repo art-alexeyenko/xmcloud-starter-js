0505a125c8214e8671f6831f3e166154c9d4e42c
Artem Alexeyenko <ala@sitecore.net>
2026-06-17 17:21:09 -0400

    use 3000 as default port

--- examples/angular-beta/src/server.ts
+++ examples/angular-beta/src/server.ts
@@ -122,10 +122,10 @@ app.use((req, res, next) => {
 
 /**
  * Start the server if this module is the main entry point
- * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
+ * The server listens on the port defined by the `PORT` environment variable, or defaults to 3000.
  */
 if (isMainModule(import.meta.url)) {
-  const port = process.env.PORT || 4000;
+  const port = process.env.PORT || 3000;
   app.listen(port, (error) => {
     if (error) {
       throw error;
